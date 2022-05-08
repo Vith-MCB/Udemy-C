610c0dd03793acb69bcf15f653d686d5c63dda5d
Vith-MCB <vitorriblacerda@gmail.com>
2022-05-08 12:42:28 -0300

    Pointers review

---
+++ Projects/Pointers/pointersBasics.c
@@ -0,0 +1,24 @@
+#import <stdio.h>
+
+/*Pointers
+ * Declaration: *examplePointer
+ * Uses: Store adress from another variable (it needs to be already declared)
+ * Example:
+ * int *pNumber = NULL, variable1; (integer pointer defined null and variable)
+ * pNumber = &variable1=10; (the pointer stores the adress from the variable)
+ * printf("%d", *pNumber); (prints the "variable1" value = 10)
+ * printf("%d", pNumber); (prints the adress)
+*/
+
+int main(){
+
+//    //To run te example, just select the code and press "ctrl+/"
+//    int *pNumber = NULL, variable1=10;
+//    pNumber = &variable1;
+//    printf("%d\n", *pNumber);
+//    printf("%p\n", pNumber);
+
+    
+
+}
+
